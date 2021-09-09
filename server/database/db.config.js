ad7eb21075e4d2887ad1191135075e9f3db4dda2
Ignacio Cabral <ignaciocabral97@gmail.com>
2021-09-09 18:28:57 -0300

    added logging:false

--- server/database/db.config.js
+++ server/database/db.config.js
@@ -1,4 +1,6 @@
 //DATABASE CONNECTION 
 const Sequelize = require('sequelize');
 require('dotenv').config();
-module.exports = new Sequelize(`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`);
+module.exports = new Sequelize(`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,{
+    logging:false //SACA LOS EXECUT DEFAULT DE LA CONSOLA
+});
